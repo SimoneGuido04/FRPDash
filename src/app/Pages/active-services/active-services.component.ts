deeb9575e84a0c891cab98fb0f9ffe42f7333465
SimoneGuido04 <guidosimone02@gmail.com>
2024-05-29 14:51:18 +0200

    Implemented basic sidenav component and created services page.

--- src/app/Pages/active-services/active-services.component.ts
+++ src/app/Pages/active-services/active-services.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {AuthService} from "../../Services/auth/auth.service";
+import {SidenavComponent} from "../sidenav/sidenav.component";
 
 @Component({
   selector: 'app-active-services',
   standalone: true,
-  imports: [],
+  imports: [
+    SidenavComponent
+  ],
   templateUrl: './active-services.component.html',
   styleUrl: './active-services.component.css'
 })
--- src/app/Pages/sidenav/sidenav.component.ts
+++ src/app/Pages/sidenav/sidenav.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDrawerContainer, MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDrawerContainer,MatSidenavModule, MatButtonModule
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
+  opened: boolean = true
+
+  constructor() {
+
+
+  }
+
 
 }
